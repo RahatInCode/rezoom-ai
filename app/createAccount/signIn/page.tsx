b63adcae0e9fd5327f0f3a02614d96877873130a
fouzia65 <fouziarahman1234567890@gmail.com>
2025-09-19 20:52:30 +0600

    added signIn and signUp page design

---
+++ app/createAccount/signIn/page.tsx
@@ -0,0 +1,28 @@
+import Link from "next/link";
+import { FcGoogle } from "react-icons/fc";
+export default function SignIn(){
+   return <div className="text-black w-[50%] space-y-12   p-8 m-auto mt-8">
+           <div>
+               <h1 className="text-3xl font-bold">Welcome ,</h1>
+               <p className="font-bold text-gray-400">sign In to Continue ! </p>
+           </div>
+           <div className="space-y-8">
+               <div className="space-y-3">
+                   
+                   <div className="form">
+                       <input type="email" className=" textbox w-full " placeholder="" />
+                       <label htmlFor="" className="label-box   text-gray-400 ">email</label>
+                   </div>
+                   <div className="form">
+                       <input type="password" className=" textbox w-full" placeholder="" />
+                       <label htmlFor="" className="label-box  text-gray-400  ">Password</label>
+                   </div>
+               </div>
+               <div className="flex flex-col gap-2">
+                   <button className=" transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  p-2 rounded-2xl">Register</button>
+                   <button className="flex justify-center items-center button3 absolute gap-2"><FcGoogle size={20} />Continue with google</button>
+               </div>
+           </div>
+           <p className="text-center font-bold text-gray-400">I am a new user <Link href={'/createAccount/signUp'}><span className="text-orange-500 ">Register</span></Link></p>
+       </div>
+}
