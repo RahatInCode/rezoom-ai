e871d35f9030740c506da09faff2b275276f5b9e
Abir-Hasan-52 <abirhasan5208+github@gmail.com>
2025-09-20 22:49:50 +0600

    ADD: ReviewSection

--- app/Components/AIChatSection/AIChatSection.tsx
+++ app/Components/AIChatSection/AIChatSection.tsx
@@ -63,9 +63,7 @@ const AIChatSection: React.FC = () => {
                 </div>
             </div>
 
-            {/* Background Circles */}
-            {/* <div className="absolute top-0 left-0 w-72 h-72 bg-purple-100 rounded-full -translate-x-1/2 -translate-y-1/2 opacity-50"></div>
-            <div className="absolute bottom-0 right-0 w-96 h-96 bg-blue-100 rounded-full translate-x-1/2 translate-y-1/2 opacity-50"></div> */}
+            
         </section>
     );
 };
