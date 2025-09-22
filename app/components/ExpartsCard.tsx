afbb67b4af5280221a94360f86041fbb86717fac
Ibrahim752006 <mdibrahim752006@gmail.com>
2025-09-23 01:30:19 +0600

    fix img tagg to Image

--- app/components/ExpartsCard.tsx
+++ app/components/ExpartsCard.tsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react';
 
 const ExpartsCard = ({people}) => {
     return (
         <div className="w-full hover:-translate-y-0.5 transition duration-300">
-            <img className="rounded-4xl" src={people.image} alt="Person_Image" />
+            <Image className="rounded-4xl" src={people.image} alt="Person_Image" />
             <h3 className="text-[#FC9667] text-xl  font-medium mt-3">{people.name}</h3>
             <p className="text-sm text-indigo-600 font-medium mt-1">{people.position}</p>
         </div>
