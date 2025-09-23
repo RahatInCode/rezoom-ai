3b3c2fb11a88f0160758d91f74a0746588ecbcd4
Ibrahim752006 <mdibrahim752006@gmail.com>
2025-09-23 21:36:23 +0600

    fix bugs

--- app/components/ExpartsCard.tsx
+++ app/components/ExpartsCard.tsx
@@ -1,10 +1,11 @@
+"use client"
 import Image from 'next/image';
 import React from 'react';
 
 const ExpartsCard = ({people}) => {
     return (
-        <div className="w-full hover:-translate-y-0.5 transition duration-300">
-            <Image className="rounded-4xl" src={people.image} alt="Person_Image" />
+        <div className="w-full h-full p-3 shadow-xl hover:-translate-y-0.5 transition duration-300">
+            <Image className=" object-contain h-96 w-full " src={people.image} fill  alt="Person_Image" />
             <h3 className="text-[#FC9667] text-xl  font-medium mt-3">{people.name}</h3>
             <p className="text-sm text-indigo-600 font-medium mt-1">{people.position}</p>
         </div>
