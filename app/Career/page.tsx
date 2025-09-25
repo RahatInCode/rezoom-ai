33cd6995dd0c2a7ccd20cd85f7ce68a75b92f32e
ahmed <ahmedrahat9901@gmail.com>
2025-09-25 22:51:44 +0600

    more commits

--- app/Career/page.tsx
+++ app/Career/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import { IoChevronUp } from "react-icons/io5";
-
+import { IoChevronDown } from "react-icons/io5";
 import Image from "next/image";
 import ExpartsCard from "../Components/ExpartsCard";
 
