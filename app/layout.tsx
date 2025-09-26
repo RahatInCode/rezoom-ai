4baff807f038da7dfcd87093c37a4afd63b346d1
ahmed <ahmedrahat9901@gmail.com>
2025-09-27 01:52:51 +0600

    fixed layout issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import { RootProvider } from "./context/createContext";
-import Navbar from "./components/Navigation/Navbar";
-import Footer from './components/Footer/Footer'
+import Navbar from "./Components/Navigation/Navbar";
+import Footer2 from "./Components/Footer/Footer";
+
 
 const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
@@ -16,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <RootProvider>
         <main className="w-full min-h-screen">{children}</main>
         </RootProvider>
-        <Footer />
+        <Footer2 />
       </body>
     </html>
     
