393e44cbb78d4d39fe5f03f7fd83751dcdb41412
Suprotik007 <csuprotik@gmail.com>
2025-09-28 21:39:21 +0600

    Created linkedIn summery generator form

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,14 +1,19 @@
 'use client'
 import React from 'react';
 import CoverLetterGenerator from './ai-cover-letter/page';
-import LinkedINpage from './linkedIn-generator/page';
+import LinkedInSummaryGenerator from './linkedIn-generator/page';
+import Lottie from 'lottie-react';
+import coverLetter from '../../public/lotties/coverLetter.json'
 
 const page = () => {
     return (
         <div>
 
-           <CoverLetterGenerator></CoverLetterGenerator>
-           <LinkedINpage></LinkedINpage>
+           <div className='max-w-7xl mx-auto px-4 md:px-10 py-10 grid md:grid-cols-2 gap-10 items-center text-center '>
+            <CoverLetterGenerator></CoverLetterGenerator>
+                <Lottie animationData={coverLetter}  loop={true} />
+           </div>
+           <LinkedInSummaryGenerator></LinkedInSummaryGenerator>
 
         </div>
     );
