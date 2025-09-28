731ec92d8f169f9dab387213c0e9bd1b16b67542
Suprotik007 <csuprotik@gmail.com>
2025-09-28 13:26:30 +0600

    created cover letter form

---
+++ app/ai/linkedIn-generator/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LinkedINpage = () => {
+    return (
+        <div className='text-black'>
+            linkedIn generator page
+        </div>
+    );
+};
+
+export default LinkedINpage;
--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 import React from 'react';
 import CoverLetterGenerator from './ai-cover-letter/page';
+import LinkedINpage from './linkedIn-generator/page';
 
 const page = () => {
     return (
         <div>
 
            <CoverLetterGenerator></CoverLetterGenerator>
+           <LinkedINpage></LinkedINpage>
+
         </div>
     );
 };
