2fa4a7f0761c944442af04ddba158b623ebc65bd
fouzia65 <fouziarahman1234567890@gmail.com>
2025-10-02 16:35:40 +0600

    make some changes in layout file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import "./globals.css";
 import { Geist, Geist_Mono } from "next/font/google";
 import { RootProvider } from "./context/createContext";
+import Navbar from "./Components/Navigation/Navbar";
+import Footer2 from "./Components/Footer/Footer";
 
-import Navbar from "./components/Navigation/Navbar";  //footer
-import Footer2 from "./components/Footer/Footer";   //navbar
+
 
 const geistSans = Geist({ subsets: ["latin"], variable: "--font-geist-sans" });
 const geistMono = Geist_Mono({ subsets: ["latin"], variable: "--font-geist-mono" });
@@ -13,11 +14,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
    
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="font-sans">
-        <Navbar />
         <RootProvider>
+        <Navbar />
         <main className="w-full min-h-screen">{children}</main>
-        </RootProvider>
         <Footer2 />
+        </RootProvider>
       </body>
     </html>
   );
