fea363ba132fe394cddc6cd9d1486581a071aa3c
Suprotik007 <csuprotik@gmail.com>
2025-10-04 22:50:00 +0600

    fixed openai config issue

--- app/api/generate-cover-letter/route.ts
+++ app/api/generate-cover-letter/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { openai } from "../../../Backend/config/OpenAI"
+import { openai } from "../../utils/OpenAI"
 
 export async function POST(req: NextRequest) {
   try {
