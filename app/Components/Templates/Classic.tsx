5718ed533b52f110ed4be6eb072d9ee6301766b6
Ibrahim752006 <mdibrahim752006@gmail.com>
2025-10-19 17:50:24 +0600

    Fix resume-template page

--- app/Components/Templates/Classic.tsx
+++ app/Components/Templates/Classic.tsx
@@ -1,9 +1,26 @@
+import { PhoneCall } from 'lucide-react';
 import React from 'react';
 
 const Classic = () => {
     return (
-        <div>
-            clasic card this is
+        <div className='w-full p-12 '>
+            <h1 className='text-center text-5xl font-bold'>Your Name</h1>
+            <h3 className='text-2xl text-black text-center'>Your Role</h3>
+
+            <div className='flex items-center justify-between mt-5'>
+                <div className='flex justify-center items-center'>
+                    <PhoneCall />
+                    <p>Number here</p>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <PhoneCall />
+                    <p>Number here</p>
+                </div>
+                <div className='flex justify-center items-center'>
+                    <PhoneCall />
+                    <p>Number here</p>
+                </div>
+            </div>
         </div>
     );
 };
